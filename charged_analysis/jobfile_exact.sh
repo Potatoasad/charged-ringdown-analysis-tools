58caef61d6609a6607759f6cde0d278b2bfde835
Asad Hussain <asadhussain625@gmail.com>
2022-06-26 23:01:06 -0500

    config files now point to correct data folder

--- charged_analysis/jobfile_exact.sh
+++ charged_analysis/jobfile_exact.sh
@@ -5,6 +5,6 @@ events_folder="./Config_files/*.ini"
 for file in ./Config_files/*ini 
 do
 	echo ${file}
-	python -Wignore analysis.py -c ${file} --diagnosticsonly
-	python -Wignore analysis.py -c ${file} --charged --exactcharge
+	#python -Wignore analysis.py -c ${file} --diagnosticsonly
+	python -Wignore analysis.py -c ${file} --skipdiagnostics --charged --exactcharge
 done
